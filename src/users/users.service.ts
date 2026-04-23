e031c5ab482e80a6d7b5f975a017f306e50bae8d
macsoftechnologies@gmail.com <macsoftechnologies@gmail.com>
2026-04-23 16:26:37 +0530

    ekyc status update to pending when adding certificate

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1703,6 +1703,11 @@ export class UsersService {
       }
       const add = await this.certificateModel.create(req);
       if (add) {
+        await this.userModel.updateOne({userId: req.userId},{
+          $set: {
+            ekyc_status: EKYCstatus.PENDING
+          }
+        })
         return {
           statusCode: HttpStatus.OK,
           message: 'Certificate added successfully',
