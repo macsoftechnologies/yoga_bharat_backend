5ca563b72b5a56809af60a41e77a02efa6f59011
macsoftechnologies@gmail.com <macsoftechnologies@gmail.com>
2026-05-01 11:34:39 +0530

    booking model and order alert model injected in passed order module.ts

--- src/passed_orders/passed_orders.module.ts
+++ src/passed_orders/passed_orders.module.ts
@@ -3,11 +3,15 @@ import { PassedOrdersService } from './passed_orders.service';
 import { PassedOrdersController } from './passed_orders.controller';
 import { MongooseModule } from '@nestjs/mongoose';
 import { PassedOrders, passedOrderSchema } from './schema/passed_orders.schema';
+import { Booking, bookingSchema } from 'src/booking/schema/booking.schema';
+import { OrderAlert, orderAlertSchema } from 'src/booking/schema/order_alert.schema';
 
 @Module({
   imports: [
     MongooseModule.forFeature([
       { name: PassedOrders.name, schema: passedOrderSchema },
+      { name: Booking.name, schema: bookingSchema },
+      { name: OrderAlert.name, schema: orderAlertSchema },
     ]),
   ],
   controllers: [PassedOrdersController],
